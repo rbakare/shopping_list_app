bb2c755c06558c78cdef4522c8b07c7387228748
Raheem Bakare <raheem@raheems-macbook-pro.local>
2018-10-18 19:17:05 -0400

    Initial Commit

---
+++ shopping_list_app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  shopping_list_app
+//
+//  Created by Raheem Bakare on 2018-10-18.
+//  Copyright © 2018 centennial college. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
